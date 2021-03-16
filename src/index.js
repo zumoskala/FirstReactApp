7eedbac823ede36137039f5869f9f75376974e3f
Zuzanna <zuzannamoskala@gmail.com>
2021-03-16 23:58:37 +0100

    first react app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
+import faker from 'faker';
+
+const App = () => {
+    return (
+        <div className={"ui container comments"}>
+            <ApprovalCard>
+                <CommentDetail author={"Sam"} timeAgo={"Today at 4:45PM"}
+                               content={"Lorem ipsum dolor sit amet, consectetur adipiscing elitt!"}
+                               avatar={faker.image.animals()}/>
+            </ApprovalCard>
+            <ApprovalCard>
+
+                <CommentDetail author={"Alex"} timeAgo={"Today at 6:00PM"}
+                               avatar={faker.image.abstract()}
+                               content={"Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. "}/>
+            </ApprovalCard>
+            <ApprovalCard>
+
+                <CommentDetail author={"Jane"} timeAgo={"Today at 7:12PM"}
+                               avatar={faker.image.people()}
+                               content={"Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"}/>
+            </ApprovalCard>
+        </div>
+    );
+};
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <App/>
+    , document.querySelector('#root'));
